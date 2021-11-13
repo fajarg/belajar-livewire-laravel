9892444f6862dae277f31dd19b443e8fb7eaee37
fajarg <fajargunawan712@gmail.com>
2021-11-13 13:14:48 +0700

    edit view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
 
     <title>Laravel</title>
 
-    <!-- Fonts -->
+    <!-- fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     <!-- Styles -->
